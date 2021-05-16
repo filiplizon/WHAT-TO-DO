f86fbb2c08e3e46d3f907858c4522e0715f147aa
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-17 00:44:22 +0200

    add base theme

---
+++ src/theme/theme.js
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+  },
+  fonts: {
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    regular: 400,
+    bold: 700,
+  },
+  styles: {
+    root: {
+      fontFamily: 'body',
+      fontWeight: 'regular',
+    },
+  },
+  breakpoints: ['360px', '768px', '1024px'],
+};
