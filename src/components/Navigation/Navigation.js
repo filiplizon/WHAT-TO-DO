0ce3f226595d2ca2f84b722a79e846c8512877ca
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-17 20:57:07 +0200

    refactor sx props

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Text, Input, IconButton, Box } from 'theme-ui';
+import { Grid, Text, Input, Box, Button } from 'theme-ui';
 import { AiFillEyeInvisible as EyeIcon } from 'react-icons/ai';
 
 const Navigation = () => (
@@ -15,7 +15,7 @@ const Navigation = () => (
       alignItems: 'center',
       justifyItems: 'center',
       backgroundColor: 'background',
-      boxShadow: '0 10px 30px -10px hsla(0, 0%, 0%, 0.1)',
+      boxShadow: 'primary',
       zIndex: 1000,
     }}
   >
@@ -42,27 +42,17 @@ const Navigation = () => (
         outlineColor: 'primary',
       }}
     />
-    <IconButton
+    <Button
       title="Hide completed"
       sx={{
         width: ['40px', null, '45px', '50px'],
         height: ['40px', null, '45px', '50px'],
         justifySelf: [null, null, 'flex-end'],
         fontSize: [5, 7],
-        p: '5px',
-        color: 'background',
-        backgroundColor: 'primary',
-        borderRadius: '50%',
-        outlineColor: 'primary',
-        cursor: 'pointer',
-        transition: 'transform .2s',
-        '&:hover': {
-          transform: 'scale(1.1)',
-        },
       }}
     >
       <EyeIcon />
-    </IconButton>
+    </Button>
   </Grid>
 );
 
