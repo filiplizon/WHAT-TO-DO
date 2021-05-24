de32c1ce6c7a286692065eed9b0b613931003736
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-24 12:59:43 +0200

    add atoms and selectors

---
+++ src/atoms/atoms.js
@@ -0,0 +1,80 @@
+import { atom, selector } from 'recoil';
+
+export const toDosState = atom({
+  key: 'toDosState',
+  default: selector({
+    key: 'toDosSelector',
+    get: async () => {
+      const response = await fetch('https://gorest.co.in/public-api/todos');
+      const { data } = await response.json();
+      return data;
+    },
+  }),
+});
+
+export const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All',
+});
+
+export const searchInputValueState = atom({
+  key: 'searchInputValue',
+  default: '',
+});
+
+export const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(toDosState);
+    const inputValue = get(searchInputValueState);
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter(
+          ({ completed, title }) => completed && title.includes(inputValue.toLowerCase()),
+        );
+      case 'Show Uncompleted':
+        return list.filter(
+          ({ completed, title }) => !completed && title.includes(inputValue.toLowerCase()),
+        );
+      case 'Search by content':
+        return list.filter(({ title }) => title.toLowerCase().includes(inputValue.toLowerCase()));
+      default:
+        return list;
+    }
+  },
+});
+
+export const toDoListDetailsState = selector({
+  key: 'todoListStatsState',
+  get: ({ get }) => {
+    const todoList = get(toDosState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter(({ completed }) => completed).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+    };
+  },
+});
+
+export const currentItemState = atom({
+  key: 'currentItemState',
+  default: {},
+});
+
+export const charCountState = atom({
+  key: 'charCountState',
+  default: selector({
+    key: 'charCountSelector',
+    get: ({ get }) => {
+      const text = get(currentItemState);
+
+      return text.length;
+    },
+  }),
+});
