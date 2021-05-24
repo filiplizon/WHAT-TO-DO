4f3f3ed36a5c04d8f10bc0764f03e342775a2f3c
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-24 12:25:29 +0200

    add details panel, feat search option

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,59 +1,70 @@
-import React from 'react';
-import { Grid, Text, Input, Box, Button } from 'theme-ui';
-import { AiFillEyeInvisible as EyeIcon } from 'react-icons/ai';
+import React, { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { searchInputValueState, todoListFilterState } from 'atoms/atoms';
+import { Grid, Input, Button } from 'theme-ui';
+import DetailsPanel from 'components/DetailsPanel/DetailsPanel';
+import { BsArrowDown as ArrowIcon } from 'react-icons/bs';
+import Logo from 'components/Logo/Logo';
 
-const Navigation = () => (
-  <Grid
-    as="nav"
-    sx={{
-      width: '100vw',
-      height: '10vh',
-      position: 'fixed',
-      top: 0,
-      px: '3vw',
-      gridTemplateColumns: ['0.3fr 0.7fr 0.25fr', null, 'repeat(3, 1fr)'],
-      alignItems: 'center',
-      justifyItems: 'center',
-      backgroundColor: 'background',
-      boxShadow: 'primary',
-      zIndex: 1000,
-    }}
-  >
-    <Box
-      sx={{
-        fontWeight: 'bold',
-        fontSize: [3, null, 4, 5],
-        justifySelf: [null, null, 'flex-start'],
-      }}
-    >
-      <Text>WHAT </Text>
-      <Text
+const Navigation = () => {
+  const [isOpen, setDetailsBoxOpen] = useState(false);
+  const setSearchInput = useSetRecoilState(searchInputValueState);
+  const setFilter = useSetRecoilState(todoListFilterState);
+
+  const handleInputChange = ({ target: { value } }) => {
+    setSearchInput(value);
+  };
+
+  const updateFilter = ({ target: { name } }) => {
+    setFilter(name);
+  };
+
+  return (
+    <>
+      <DetailsPanel isOpen={isOpen} />
+      <Grid
+        as="nav"
         sx={{
-          color: 'primary',
+          width: '100vw',
+          height: '10vh',
+          position: 'fixed',
+          top: 0,
+          px: '3vw',
+          gridTemplateColumns: ['0.3fr 0.7fr 0.25fr', null, 'repeat(3, 1fr)'],
+          alignItems: 'center',
+          justifyItems: 'center',
+          backgroundColor: 'background',
+          boxShadow: 'primary',
+          zIndex: 1000,
         }}
       >
-        TO DO
-      </Text>
-    </Box>
-    <Input
-      placeholder="Search"
-      sx={{
-        fontSize: [2, null, 4],
-        outlineColor: 'primary',
-      }}
-    />
-    <Button
-      title="Hide completed"
-      sx={{
-        width: ['40px', null, '45px', '50px'],
-        height: ['40px', null, '45px', '50px'],
-        justifySelf: [null, null, 'flex-end'],
-        fontSize: [5, 7],
-      }}
-    >
-      <EyeIcon />
-    </Button>
-  </Grid>
-);
+        <Logo />
+        <Input
+          name="Search by content"
+          onClick={updateFilter}
+          onChange={handleInputChange}
+          placeholder="Search"
+          sx={{
+            width: ['100%', null, null, null, '60%'],
+            fontSize: [2, null, 4],
+          }}
+        />
+        <Button
+          onClick={() => setDetailsBoxOpen(!isOpen)}
+          sx={{
+            width: ['40px', null, '45px', '50px'],
+            height: ['40px', null, '45px', '50px'],
+            borderRadius: '50%',
+            fontSize: [5, 7],
+            transform: isOpen ? 'rotate(180deg)' : 'rotate(0)',
+            transition: 'transform .3s ease-in-out',
+          }}
+        >
+          <ArrowIcon />
+        </Button>
+      </Grid>
+    </>
+  );
+};
 
 export default Navigation;
