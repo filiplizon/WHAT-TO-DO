12a7f75099fd7d0f6d91a4272d87c39882c4c391
Filip Lizoń <filip.lizon1@gmail.com>
2021-05-30 23:35:19 +0200

    update page style

--- src/views/ItemPage/ItemPage.js
+++ src/views/ItemPage/ItemPage.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { currentItemState } from 'atoms/atoms';
-import { Container, Flex, Paragraph, Image, Heading } from '@theme-ui/components';
-import CloseButton from 'components/CloseButton/CloseButton';
-import Illustration from 'assets/images/illustration.svg';
+import { toDosState } from 'atoms/atoms';
+import { Container, Flex, Paragraph, Heading } from '@theme-ui/components';
 import Logo from 'components/Logo/Logo';
+import CloseButton from 'components/CloseButton/CloseButton';
+import Illustration from 'components/Illustration/Illustration';
+import Wave from 'assets/images/wave.svg';
 
-const ItemPage = () => {
+const ItemPage = ({
+  match: {
+    params: { id },
+  },
+}) => {
   const [redirect, setRedirect] = useState(false);
-  const currentItem = useRecoilValue(currentItemState);
+  const [item, setItem] = useState({});
+  const toDoList = useRecoilValue(toDosState);
+
+  useEffect(() => {
+    const currentItem = toDoList.find((item) => item.id == id);
+    setItem(currentItem);
+  }, [item]);
 
   const handlePageClose = () => {
     setRedirect(!redirect);
@@ -25,8 +36,8 @@ const ItemPage = () => {
         sx={{
           width: '100%',
           justifyContent: 'space-between',
-          position: 'absolute',
-          top: ['15px', null, '25px', '35px', '45px'],
+          position: 'relative',
+          top: ['15px', null, '25px', '35px', '20px'],
           px: '3vw',
         }}
       >
@@ -35,72 +46,88 @@ const ItemPage = () => {
       </Flex>
       <Flex
         sx={{
-          height: '100vh',
+          height: ['95vh', null, null, null, '100%'],
           width: '100%',
           px: '3vw',
           pt: ['2vh', null, null, null, '10vh'],
           pb: ['2vh', null, null, null, '10vh'],
           alignItems: 'center',
-          overflowX: 'hidden',
+          overflow: 'hidden',
         }}
       >
         <Flex
           sx={{
-            position: 'relative',
-            left: [null, null, null, null, '5%'],
-            height: ['65%', null, null, null, '100%'],
+            height: '80%',
             width: '100%',
-            flexDirection: ['column', null, null, null, 'row'],
+            flexDirection: 'column',
             justifyContent: 'space-between',
-            alignItems: 'center',
+            alignItems: ['center', null, null, null, 'flex-start'],
             textAlign: [null, null, null, null, 'center'],
           }}
         >
+          <Container
+            sx={{
+              position: [null, null, null, null, 'absolute'],
+              top: [null, null, null, null, '15%'],
+              width: 'auto',
+              p: '10px',
+              m: 0,
+              border: (theme) => `2px solid ${theme.colors.primary}`,
+              alignSelf: 'flex-start',
+              fontSize: [1, 2, 3, 5, 2],
+              textAlign: 'left',
+            }}
+          >
+            <Paragraph sx={{ mb: '10px' }}>
+              Created at: {new Date(item.created_at).toLocaleString('pl-PL')}
+            </Paragraph>
+            <Paragraph>Completed: {item.completed ? 'Yes' : 'No'}</Paragraph>
+          </Container>
           <Flex
             sx={{
-              width: [null, null, null, null, '620px'],
-              height: [null, null, null, null, '450px'],
-              position: 'relative',
-              p: [null, null, null, null, '120px 30px 0'],
-              borderRadius: '50%',
+              position: [null, null, null, null, 'absolute'],
+              top: [null, null, null, null, '50%'],
+              width: 'auto',
               flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: [null, null, null, null, 'center'],
-              bg: [null, null, null, null, 'primary'],
-              color: [null, null, null, null, 'background'],
+              alignItems: 'flex-start',
+              transform: [null, null, null, null, 'translateY(-50%)'],
             }}
           >
             <Heading
               as="h2"
               sx={{
-                fontSize: [4, 5, 6, 7, 6],
-                color: ['primary', null, null, null, 'background'],
+                fontSize: [4, 5, null, 6, 5],
+                color: 'primary',
               }}
             >
-              {currentItem.name ? currentItem.name : 'To Do'}
+              {item.name ? item.name : 'TO DO'}
             </Heading>
             <Paragraph
               sx={{
                 height: '70%',
-                width: ['100%', null, '80%'],
-                mt: '30px',
-                fontSize: [null, 3, 4, 5, 4],
-                overflow: 'hidden',
+                width: [null, null, null, null, '60%'],
+                mt: '15px',
+                fontSize: [null, 3, 4, 5, 3],
+                textAlign: 'left',
               }}
             >
-              {currentItem.title}
+              {item.title}
             </Paragraph>
           </Flex>
-          <Container
-            sx={{
-              width: ['100%', null, null, null, 'primary'],
-              opacity: 0.4,
-            }}
-          >
-            <Image sx={{ width: ['100%', null, null, null, '60%'] }} src={Illustration} />
-          </Container>
+          <Illustration />
         </Flex>
       </Flex>
+      <Container
+        sx={{
+          display: ['none', null, null, null, 'block'],
+          height: '30%',
+          width: '100vw',
+          position: 'absolute',
+          bottom: 0,
+          backgroundImage: `url(${Wave})`,
+          backgroundSize: 'cover',
+        }}
+      />
     </>
   );
 };
